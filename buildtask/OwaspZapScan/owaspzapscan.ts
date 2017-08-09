b28f87382a26dd51c122ae6dcdcc5c62ca7f4a4f
Kasun Kodagoda <kvkrusl@gmail.com>
2017-08-09 20:56:41 +0530

    Removed zaproxy package from the project

--- buildtask/OwaspZapScan/owaspzapscan.ts
+++ buildtask/OwaspZapScan/owaspzapscan.ts
@@ -1,7 +1,7 @@
 import { ZapActiveScanOptions } from './ZapClientOptions';
+
 import task = require('vsts-task-lib');
 import path = require('path');
-import ZapClient = require('zaproxy');
 import request = require('request');
 
 task.setResourcePath(path.join(__dirname, 'task.json'));
