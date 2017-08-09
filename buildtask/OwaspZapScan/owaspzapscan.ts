d4d5c296b7517091f7e0de2bd7a4e9e3494cd4b3
Kasun Kodagoda <kvkrusl@gmail.com>
2017-08-09 20:41:44 +0530

    Installed request package and typings

--- buildtask/OwaspZapScan/owaspzapscan.ts
+++ buildtask/OwaspZapScan/owaspzapscan.ts
@@ -1,6 +1,7 @@
 import task = require('vsts-task-lib');
 import path = require('path');
 import ZapClient = require('zaproxy');
+import request = require('request');
 
 task.setResourcePath(path.join(__dirname, 'task.json'));
 
