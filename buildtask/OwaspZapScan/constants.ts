6f8cb7ee1edae5deedf7aa89287fcd60c1b47260
Kasun Kodagoda <kvkrusl@gmail.com>
2017-08-13 13:10:27 +0530

    Added the constants and enums file and moved the code

---
+++ buildtask/OwaspZapScan/constants.ts
@@ -0,0 +1,16 @@
+export class Constants {
+    // Report Endpoints
+    static HtmlReport: string = 'htmlreport';
+    static XmlReport: string = 'xmlreport';
+    static MdReport: string = 'mdreport';
+
+    // Report Types
+    static Html: string = 'html';
+    static Xml: string = 'xml';
+    static Markdown: string = 'md';
+
+    // Risk Code
+    static HighRisk: string = '3';
+    static MediumRisk: string = '2';
+    static LowRisk: string = '1';
+}
---
+++ buildtask/OwaspZapScan/enums.ts
@@ -0,0 +1,5 @@
+export enum ReportType {
+    XML,
+    HTML,
+    MD
+}
