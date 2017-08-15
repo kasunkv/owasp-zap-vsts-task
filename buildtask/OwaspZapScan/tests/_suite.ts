59232fb7ea01a88c2a94d2b358b10ac852b3889b
Kasun Kodagoda <kvkrusl@gmail.com>
2017-08-15 22:05:36 +0530

    Configured the project for unit tests and added a simple test to make sure everything is working.

---
+++ buildtask/OwaspZapScan/tests/_suite.ts
@@ -0,0 +1,15 @@
+import * as path from 'path';
+import * as  assert from 'assert';
+import * as mock from 'vsts-task-lib/mock-test';
+
+describe('OWASP ZAP Scan Task Tests', function() {
+    before(() => {});
+
+    after(() => {});
+
+    it('Simple Test', (done: MochaDone) => {
+        this.timeout(1000);
+
+        done();                
+    });
+});
