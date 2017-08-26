0f6cf63d3a0b29dd08dda245f9d480016dff9378
Kasun Kodagoda <kvkrusl@gmail.com>
2017-08-27 02:00:08 +0530

    Updated the IZapScan interface

--- buildtask/OwaspZapScan/interfaces/contracts/IZapScan.ts
+++ buildtask/OwaspZapScan/interfaces/contracts/IZapScan.ts
@@ -1,6 +1,8 @@
 import { ScanResult } from "../types/ScanResult";
+import { ZapScanType } from "../../enums/Enums";
 
 export interface IZapScan {
     ScanType: string;
+    ZapScanType: ZapScanType;
     ExecuteScan(): Promise<ScanResult>;
 }
