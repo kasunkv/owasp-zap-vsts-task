f5bdc97c9f5333f83727a4a9b31f887989cb2fec
Kasun Kodagoda <kvkrusl@gmail.com>
2018-07-09 21:49:50 +0530

    Added unit tests and test data

--- BuildTask/tests/helper.spec.ts
+++ BuildTask/tests/helper.spec.ts
@@ -14,7 +14,7 @@ describe('OWASP Zap Scan Helpers', function() {
         });
     });
 
-    describe('When a valid xmlReport and a url is passed, the return value', () => {
+    describe('When a valid xmlReport with two site elements and a url is passed, the return value', () => {
         let helper: Helper;
         let xmlString: string;
         let result: AlertResult;
@@ -24,6 +24,53 @@ describe('OWASP Zap Scan Helpers', function() {
         before(() => {
             helper = new Helper();
             let xmlPath = path.join(__dirname, 'valid.xml');
+            xmlString = fs.readFileSync(xmlPath, 'utf8');
+            result = helper.ProcessAlerts(xmlString, validTargetUrl);
+        });
+    
+        it('Should not be undefined', () => {            
+            expect(result).toNotBe(undefined);
+        });
+
+        it('Should not be null', () => {
+            expect(result).toNotBe(null);
+        });
+
+        it('Should contain an array of alerts',  () => {
+            expect(result.Alerts).toBeAn(Array);
+        });
+
+        it('Should contain one or many alerts',  () => {
+            expect(result.Alerts.length).toBeGreaterThan(0);
+        });
+
+        it('Should have 1 high risk alerts', () => {
+            expect(result.HighAlerts).toBe(1);
+        });
+
+        it('Should have 1 medium risk alerts', () => {
+            expect(result.MediumAlerts).toBe(1);
+        });
+
+        it('Should have 1 low risk alerts', () => {
+            expect(result.LowAlerts).toBe(1);
+        });
+
+        it('Should have 1 info risk alerts', () => {
+            expect(result.InformationalAlerts).toBe(1);
+        });
+    });
+
+    describe('When a valid xmlReport with one site elements and a url is passed, the return value', () => {
+        let helper: Helper;
+        let xmlString: string;
+        let result: AlertResult;
+        const validTargetUrl: string = 'http://k2vowasptestsite.azurewebsites.net';
+        const invalidTargetUrl: string = 'http://k2vowasptestsite-invalid.azurewebsites.net';
+    
+        before(() => {
+            helper = new Helper();
+            let xmlPath = path.join(__dirname, 'valid-one-site.xml');
             xmlString = fs.readFileSync(xmlPath, 'utf8');
             result = helper.ProcessAlerts(xmlString, validTargetUrl);
         });
